e0b6da37a66019b7c4f6e41199c3215f62fd1f7e
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-28 16:36:40 +0000

    update

--- check_postgres.py
+++ check_postgres.py
@@ -3,14 +3,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-print(os.getenv("POSTGRES_IP"))
-
-postgres_ip = '' + os.getenv("POSTGRES_IP")
-hostname = postgres_ip + ':5432'
-
-response = os.system("ping -c 1 " + hostname)
+response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
 
 while response != 0:
   print("Looking for postgres...")
   time.sleep(5)
-  response = os.system("ping -c 1 " + hostname)
+  response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
