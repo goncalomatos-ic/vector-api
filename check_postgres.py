f2c6ac83f05d0c41c7cfcce8b1424ed242e7b063
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-28 16:41:04 +0000

    small fix

--- check_postgres.py
+++ check_postgres.py
@@ -1,11 +1,13 @@
 import time
-import os
-from dotenv import load_dotenv
-load_dotenv()
+#import os
+#from dotenv import load_dotenv
+#load_dotenv()
 
-response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
+time.sleep(30)
 
-while response != 0:
-  print("Looking for postgres...")
-  time.sleep(30)
-  response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
+#response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
+
+#while response != 0:
+  #print("Looking for postgres...")
+  #time.sleep(30)
+  #response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
