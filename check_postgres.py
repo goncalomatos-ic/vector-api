759c46b1b549399f61d07052c359b2fb72cafb11
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-28 16:39:06 +0000

    fix

--- check_postgres.py
+++ check_postgres.py
@@ -7,5 +7,5 @@ response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
 
 while response != 0:
   print("Looking for postgres...")
-  time.sleep(5)
+  time.sleep(30)
   response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
