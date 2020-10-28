f901792f1082c96bbe8025ef1ae84f6b0035bd66
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-28 19:33:33 +0000

    cleanup

--- check_postgres.py
+++ check_postgres.py
@@ -1,13 +1,9 @@
 import time
-#import os
-#from dotenv import load_dotenv
-#load_dotenv()
 
+# Wait for postgres to be up and running
+#
+# Improvement: try to connect to postgres
+#
+# Note: pinging the container is not enough because
+#       it doesn't ensure that postgres is running
 time.sleep(10)
-
-#response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
-
-#while response != 0:
-  #print("Looking for postgres...")
-  #time.sleep(30)
-  #response = os.system("ping -c 1 " + os.getenv("POSTGRES_IP"))
